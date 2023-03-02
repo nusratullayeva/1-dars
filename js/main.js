aa688066d3282e7cca1d5ce8771fa28a7d9e83e1
nusratllayeva <anvarsaidmurotov@gmail.com>
2023-03-01 21:03:31 -0800

    first commit

---
+++ js/main.js
@@ -0,0 +1,42 @@
+// const firstName = Number(prompt('raqam kiriting'));
+
+// switch (firstName) {
+//     case 1:
+//         console.log('duwanba');
+//         break;
+//     case 2:
+//         console.log('sewanba');
+//         break;
+//     case 3:
+//         console.log('chorwanba');
+//         break;
+//     case 4:
+//         console.log('paywanba');
+//         break;
+//     case 5:
+//         console.log('juma');
+//         break;
+//     case 6:
+//         console.log('wanba');
+//         break;
+//     case 7:
+//         console.log('yakwanba');
+//         break;
+
+//     default:
+//         console.log(xato);
+//         break;          
+
+
+// let age = +prompt('yoshingiz')
+// const firstName = age >= 18 ? 'Siz mashina hayday olasiz' : 'Siz mashina hayday olmaysiz';
+// console.log(firstName);
+
+
+// function firstname(name, greeting) {
+//     return greeting + name;
+// }
+
+// console.log(firstname(Ezoza));
+// console.log(firstname(shaxlo));
+// console.log(firstname(Mubina));
